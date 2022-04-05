6bf0eeb5bee122a68724422102e33eb24e6890ce
Molly Collins <molly.c.collins@gmail.com>
2022-04-05 17:32:12 -0500

    Assignment update

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,79 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+// Required Features
+// Update the cart.js file to do the following:
+
+// Create a global variable named basket and set it to an empty array.
+let basket = [];
+// Create a function called addItem. It should:
+// take an input parameter for a string item
+// add the new item to the global array basket.
+// return true indicating the item was added
+
+function addItem(grocery){
+console.log('in addItems');
+basket.push(grocery);
+return true;
+}
+console.log(addItem('Avocados'));
+console.log(addItem('Butter' ));
+console.log(addItem('Cheese' ));
+console.log(addItem('Donuts' ));
+console.log(addItem('Eggs' ));
+
+// // Create a function called listItems. It should:
+// // loop over the items in the basket array
+
+
+function listItems(){
+    console.log('in listItems');
+    for( i=0; i>basket.length; i++);
+    console.log(basket[i]);
+    
+    }
+    listItems();
+// Create a function called empty. It should:
+// reset the basket to an empty array
+function empty(){
+console.log('in empty');
+basket = [];
+}
+
+// IMPORTANT Make sure that you are writing code in the file to test every function that you write!
+
+// For example, to test addItem:
+
+console.log(`Basket is ${basket}`);
+// console.log('Adding apples (expect true)', addItem('apples'));
+// console.log(`Basket is now ${basket}`);
+
+
+// Stretch Goals
+// Remember that Stretch Goals are not required, but will help you to further develop concepts from the skills we have covered.
+
+// Using functions in other functions!
+
+// Add a global const named maxItems and set it to 5.
+const maxItems = 5;
+
+// Create a function called isFull(). It should:
+// return false if the basket contains less than max number of items
+// return true otherwise (equal or more than maxItems)
+//function isFull(){
+    //if( i=0; i<basket.length; i++)
+   // return fasle;
+//}
+
+// Update the required addItem function to:
+// Use the isFull function to prevent more than maxItems from being added to the basket.
+// If an item was added to the array, return true
+// If there was no room and the item could not be added return false
+// Using Array built-in functions!
+
+// Create a function called removeItem. It should:
+// Take an input parameter for a string item
+// Use Array.indexOf to find the index of the first matching item in the basket.
+// Use Array.splice to remove the first matching item from the basket.
+// Return the item removed or null if the item was not found
